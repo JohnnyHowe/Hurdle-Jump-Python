c61b5c4ba4153ea1387ae564d9ae617ffec36cb2
Johnny Howe <jonathon.o.howe@gmail.com>
2020-06-29 14:35:14 +1200

    Completely re-done

--- main.py
+++ main.py
@@ -1,273 +1,123 @@
-from pygame import *
-import random
+import pygame
+from player import Player  # Import our Player class
+from hurdle_handler import Hurdle_Handler
 
-windowX = 720
-windowY = 360
-
-init()
-clock = time.Clock()
-window = display.set_mode((windowX, windowY))
-
-display.set_caption('Jumpy Thing!')
-
-ground = windowY - 70
-
-gravity = 1
+WINDOW_SIZE = (720, 360)
+GROUND_Y = 280
 
 
-class PlayerClass:
-    def __init__(self, scale, imageChangeSpeed, terminalVelocity):
-        self.run1 = transform.scale(image.load('run2.png'), (7 * scale, 14 * scale))
-        self.run2 = transform.scale(image.load('run3.png'), (7 * scale, 14 * scale))
-        self.run3 = transform.scale(image.load('run4.png'), (7 * scale, 14 * scale))
-        self.run4 = transform.scale(image.load('run5.png'), (7 * scale, 14 * scale))
-        self.run5 = transform.scale(image.load('run6.png'), (7 * scale, 14 * scale))
+def event_handler():
+    """ Returns all pygame events.
+    Checks and handles the QUIT event.
+    Returns:
+        events (list): pygame events
+    """
+    events = pygame.event.get()
+    for event in events:
 
-        self.scale = scale
-        self.imageChangeSpeed = imageChangeSpeed
-        self.terminalVelocity = terminalVelocity
-
-        self.height = 14 * scale
-        self.width = 7 * scale
-
-    dead = False
-
-    def update(self):
-        self.physics()
-
-        if self.touchingHurdle():
-            self.dead = True
-
-        if not self.dead:
-            self.playerInput()
-
-        self.y += self.velocityY
-
-        self.show()
-
-    def touchingHurdle(self):
-        for hurdle in hurdleManager.hurdleList:
-            if self.x + self.width > hurdle.x:
-                if self.x < hurdle.x + hurdle.width:
-                    if self.y + self.height > hurdle.y:
-                        return True
-
-    x = 100
-    y = 100
-
-    velocityY = 0
-
-    def playerInput(self):
-        pressedKeys = key.get_pressed()
-
-        if pressedKeys[K_SPACE]:
-            if self.y + self.height == ground:
-                self.velocityY -= 10
-            else:
-                self.velocityY -= gravity / 2
-
-    def physics(self):
-        if self.dead:
-            if self.y < windowY:
-                self.velocityY += 1
+        # Has the quit button been pressed?
+        if event.type == pygame.QUIT:
+            quit()
 
 
-        elif self.y + self.height < ground:
-            if self.velocityY < self.terminalVelocity:
-                self.velocityY += gravity
+def show_ground(window, primary_color=(255, 117, 10), secondary_color=(219, 90, 0), surface_height=15):
+    """ Show an orange rectange where the ground is.
+    Parameters:
+        window (pygame.Surface): Window to show rect on
+        primary_color (tuple): Main color of the ground
+        secondary_color (tuple): Color of the surface of the ground
+        surface_height (int): Height of the surface to display
+    """
+    pygame.draw.rect(window, primary_color, (0, GROUND_Y, WINDOW_SIZE[0], WINDOW_SIZE[1] - GROUND_Y))
+    pygame.draw.rect(window, secondary_color, (0, GROUND_Y, WINDOW_SIZE[0], surface_height))
 
 
-        elif self.velocityY > 0:
-            self.velocityY = 0
-            self.y = ground - self.height
-
-    runTick = 0
-
-    def show(self):
-        if self.runTick <= self.imageChangeSpeed:
-            img = self.run1
-        elif self.runTick <= self.imageChangeSpeed * 2:
-            img = self.run2
-        elif self.runTick <= self.imageChangeSpeed * 3:
-            img = self.run3
-        elif self.runTick <= self.imageChangeSpeed * 4:
-            img = self.run4
-        else:
-            img = self.run5
-
-        self.runTick += 1
-
-        if self.runTick >= self.imageChangeSpeed * 5:
-            self.runTick = 0
-
-        window.blit(img, (self.x, self.y))
-
-
-player = PlayerClass(5, 6, 10)
-
-
-class HurdleManager:
-    def __init__(self, scale, spawnRange):
-        self.img = transform.scale(image.load('hurdle.png'), (7 * scale, 15 * scale))
-
-        self.spawnRange = spawnRange
-        self.hurdleList = []
-        self.scale = scale
-
-    def update(self, doSpawn, moveSpeed):
-        if doSpawn:
-            self.spawn()
-        self.manage(moveSpeed)
-
-    def manage(self, moveSpeed):
-        hurdles2 = []
-
-        for hurdle in self.hurdleList:
-            hurdle.update(moveSpeed)
-
-            if hurdle.onScreen():
-                hurdles2.append(hurdle)
-
-        self.hurdleList = hurdles2
-
-    spawnTick = 0
-
-    def spawn(self):
-        if self.spawnTick >= self.spawnRange[1]:
-            newHurdle = HurdleClass(windowX, self.img, 7 * self.scale, 15 * self.scale)
-            self.hurdleList.append(newHurdle)
-            self.spawnTick = 0
-
-        elif self.spawnTick > self.spawnRange[0]:
-            if random.randint(0, self.spawnRange[1] - self.spawnRange[0]) == 0:
-                newHurdle = HurdleClass(windowX, self.img, 7 * self.scale, 15 * self.scale)
-                self.hurdleList.append(newHurdle)
-                self.spawnTick = 0
-
-        self.spawnTick += 1
-
-
-hurdleManager = HurdleManager(3, (45, 90))
-
-
-class HurdleClass:
-    def __init__(self, x, img, width, height):
-        self.x = x
-        self.img = img
-        self.width = width
-        self.height = height
-        self.y = ground - height
-
-    def update(self, moveSpeed):
-        self.move(moveSpeed)
-        self.show()
-
-    def move(self, moveSpeed):
-        self.x -= moveSpeed
-
-    def show(self):
-        window.blit(self.img, (self.x, self.y))
-
-    def onScreen(self):
-        if self.x + self.width > 0:
+def has_player_hit_hurdle(player, hurdles, ground_height):
+    """ Has the player hit a hurdle?
+    Parameters:
+         player (Player): player object to check collision for
+         hurdles (list): list of hurdles to check
+    Returns:
+        touching (bool): has the player hit a hurdle?
+    """
+    player_rect = (player.pos[0] - player.size[0] / 2, player.pos[1] - player.size[1] / 2) + player.size
+    for hurdle in hurdles:
+        hurdle_rect = (hurdle.x - hurdle.size[0] / 2, ground_height - hurdle.size[1]) + hurdle.size
+        if touching(player_rect, hurdle_rect):
             return True
-        else:
-            return False
+    return False
 
 
-def mainEventLoop():
-    for events in event.get():
-        if events.type == KEYDOWN:
-            if events.key == K_ESCAPE:
-                quit()
+def touching(rect1, rect2):
+    """ Are the two rects touching?
+
+    rects to be in the form (left x, right y, width, height)
+
+    Parameters:
+        rect1 (tuple): rect to check for collision with rect2
+        rect2 (tuple): rect to check for collision with rect1
+    """
+    return (rect1[0] + rect1[2] >= rect2[0] and
+            rect1[0] <= rect2[0] + rect2[2] and
+            rect1[1] + rect1[3] >= rect2[1] and
+            rect1[1] <= rect2[1] + rect2[3])
 
 
-groundImg = transform.scale(image.load('ground.png'), (windowX, int(windowY - ground)))
-
-font1 = font.Font('SFPixelate.ttf', 50)
-font2 = font.Font('SFPixelate.ttf', 40)
-deathMessage1 = font1.render('You Fell Over!', True, (255, 255, 255))
-deathMessage1Shadow = font1.render('You Fell Over!', True, (0, 0, 0))
-deathMessage2 = font2.render('Press Space', True, (255, 255, 255))
-deathMessage2Shadow = font2.render('Press Space', True, (0, 0, 0))
-
-message1Rect = deathMessage1.get_rect()
-message1x = windowX / 2 - message1Rect.width / 2
-
-message2Rect = deathMessage2.get_rect()
-message2x = windowX / 2 - message2Rect.width / 2
+def show_text(window, text, size, pos, color=(0, 0, 0)):
+    font = pygame.font.Font("mcFont.ttf", size)
+    textSurf = font.render(text, True, color)
+    window.blit(textSurf, pos)
 
 
-def showMessage(y):
-    window.blit(deathMessage1, (message1x, y))
-    window.blit(deathMessage1Shadow, (message1x + 5, y + 5))
+def show_all(items, window, score, high_score):
+    """ Show all items.
+    it's assumed every item in items has a show(window) method. """
+    window.fill((200, 236, 254))
+    for item in items:
+        item.show(window)
+    show_ground(window)
 
-    window.blit(deathMessage2, (message2x, y + message1Rect.height))
-    window.blit(deathMessage2Shadow, (message2x, message1Rect.height + 5 + y))
+    show_text(window, str(score), 60, (10, 10), color=(219, 90, 0))
+    show_text(window, str(high_score), 60, (10, 70), color=(100, 40, 0))
 
-
-score = {'gameScore': 0}
-
-
-def game():
-    player.update()
-    while True:
-        if player.dead:
-            fall(scoreStr)
-
-        mainEventLoop()
-        window.fill((200, 240, 250))
-
-        player.update()
-
-        hurdleManager.update(True, score['gameScore'] / 50 + 3)
-        window.blit(groundImg, (0, ground))
-
-        clock.tick(60)
-
-        scoreStr = font2.render(str(round(score['gameScore'])), True, (0, 0, 0))
-        window.blit(scoreStr, (50, 50))
-
-        display.update()
-
-        score['gameScore'] += 0.1
+    pygame.display.update()
 
 
-def fall(scoreStr):
-    space = 0
-    while True:
-        pressedKeys = key.get_pressed()
+def main():
+    """ Main game function.
+    Sets up the game and calls the game loop """
+    pygame.init()
 
-        oldSpace = space
-        space = pressedKeys[K_SPACE]
+    window = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption("Jumpy Thing!")
 
-        mainEventLoop()
-        window.fill((200, 240, 250))
+    clock = pygame.time.Clock()
+    player = Player(GROUND_Y)
+    hurdle_handler = Hurdle_Handler(GROUND_Y)
 
-        player.update()
+    score = 0
+    highscore = 0
 
-        hurdleManager.update(False, score['gameScore'] / 50 + 3)
-        window.blit(groundImg, (0, ground))
+    while True:  # Game loop
+        event_handler()
+        d_time = clock.tick()
 
-        clock.tick(60)
+        score += d_time / 200
+        highscore = max(score, highscore)
 
-        showMessage(50)
+        # Update game
+        player.update(d_time)
+        player.stand(GROUND_Y)
+        hurdle_handler.update(d_time, WINDOW_SIZE[0], 1 + score / 50)
 
-        window.blit(scoreStr, (50, 50))
-        display.update()
+        # Show everything
+        show_all([player, hurdle_handler], window, int(score), int(highscore))
 
-        spaceEvent = space - oldSpace
-
-        if spaceEvent == 1:
-            # Reset Everything
-
-            hurdleManager.hurdleList = []
-            player.velocityY = 0
-            player.dead = False
-            player.y = ground - player.height
-            score['gameScore'] = 0
-
-            break
+        if has_player_hit_hurdle(player, hurdle_handler.hurdles, GROUND_Y):
+            player = Player(GROUND_Y)
+            hurdle_handler = Hurdle_Handler(GROUND_Y)
+            score = 0
 
 
-game()
+if __name__ == "__main__":  # If this is the file that was opened/run
+    main()
